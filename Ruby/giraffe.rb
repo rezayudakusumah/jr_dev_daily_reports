b0a2ee3839193e00af069458975233fac898e670
reza <sebutsajamentari@gmail.com>
2019-09-06 22:16:08 +0700

    Working with Strings

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,6 +1,30 @@
-name = "Mike" #string
-age = 30 #integer
-gpa = 3.5 #float
-ismale = true #boolean
-istall = false #boolean
-flaws = nil #null
+
+puts "Hogwarts Academy"
+puts "Hogwarts\" Academy"
+puts "Hogwarts\nAcademy"
+
+phrase = "Hogwarts Academy"
+puts phrase
+
+phrase2 = "Hogwarts Academy"
+puts phrase2.upcase()
+
+phrase3 = "Hogwarts Academy"
+puts phrase3.downcase()
+
+phrase4 = "   Hogwarts Academy   "
+puts phrase4
+
+phrase5 = "   Hogwarts Academy   "
+puts phrase5.strip()
+
+puts phrase.length()
+puts phrase4.length()
+puts phrase.include? "Academy"
+puts phrase.include? "Academys"
+puts phrase[0]
+puts phrase[8]
+puts phrase[0,3]
+puts phrase.index("A")
+puts phrase.index("rts")
+puts "Academy".upcase()
