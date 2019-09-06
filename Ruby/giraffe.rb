cd1f467158caacb59d392ef815059875aa9fe2b6
reza <sebutsajamentari@gmail.com>
2019-09-06 21:53:34 +0700

    Print variable

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -5,7 +5,11 @@ character_age = "35"
 puts ("There once was a man name " + character_name)
 puts ("he was " + character_age + " years old")
 
-character_name = "Tom"
+character_name = "Reza"
 
 puts ("He really liked the name " + character_name)
 puts ("but didn't like being " + character_age)
+
+character_age = 36
+
+puts("Next week he is " + character_age.to_s)
