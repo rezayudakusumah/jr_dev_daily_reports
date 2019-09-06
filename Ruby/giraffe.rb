ecc1b20890d49e4cdd7253145b0fb22f1e881e09
reza <sebutsajamentari@gmail.com>
2019-09-06 22:00:15 +0700

    Data types

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,15 +1,6 @@
-
-character_name = "Mike"
-character_age = "35"
-
-puts ("There once was a man name " + character_name)
-puts ("he was " + character_age + " years old")
-
-character_name = "Reza"
-
-puts ("He really liked the name " + character_name)
-puts ("but didn't like being " + character_age)
-
-character_age = 36
-
-puts("Next week he is " + character_age.to_s)
+name = "Mike" #string
+age = 30 #integer
+gpa = 3.5 #float
+ismale = true #boolean
+istall = false #boolean
+flaws = nil #null
