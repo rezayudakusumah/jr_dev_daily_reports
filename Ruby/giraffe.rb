26c864356b2b77bc085045b1edc7af05c948563a
reza <sebutsajamentari@gmail.com>
2019-09-06 21:49:10 +0700

    Update giraffe.rb

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,4 +1,11 @@
-puts "Mike"
-puts " is Cool"
-print "Mike"
-print " is Cool"
+
+character_name = "Mike"
+character_age = "35"
+
+puts ("There once was a man name " + character_name)
+puts ("he was " + character_age + " years old")
+
+character_name = "Tom"
+
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
