629202f44468477055b8c99113427387adf4e602
reza <sebutsajamentari@gmail.com>
2019-09-07 12:23:02 +0700

    Hashes

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,27 +1,9 @@
-friends = Array["Kevin", "Oscar", "Andy"]
-puts friends
+states = {
+  1 => "PA",
+  "New York" => "NY",
+  :Oregon => "OR"
+}
 
-alltype = Array[1, "Kevin", false]
-puts alltype
-
-puts friends[0]
-puts friends[-1]
-puts friends[0,2]
-
-puts friends.length()
-puts friends.include? "Oscar"
-puts friends.reverse()
-puts friends.sort()
-
-friends[0] = "Reza"
-puts friends[0]
-
-#dinamis
-teman = Array.new
-
-teman[0] = "Asep"
-teman[5] = "Cecep"
-
-puts teman[0]
-puts teman[5]
-puts teman
+puts states[1]
+puts states["New York"]
+puts states[:Oregon]
