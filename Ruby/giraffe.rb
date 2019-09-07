40d8119dc72b3c8a14f658d51b9b28be5582e5d4
reza <sebutsajamentari@gmail.com>
2019-09-07 10:46:06 +0700

    Getting input using platformio-ide-terminal

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,39 +1,14 @@
-puts 5
-puts 5.5
-puts 5.53543
-puts -5.45345
-puts 5 + 2
-puts 5 - 2
-puts 5 / 2
-puts 5 * 2
-puts 10 % 3
-puts 5 ^ 2
-puts 5**2
+puts "Enter your name: "
+name = gets()
+puts "Enter your age: "
+age = gets()
 
-num = 9
-puts num
-puts ("My Number " + num.to_s) #must convert to string
+puts "Enter your name: "
+name1 = gets().chomp()
+puts "Enter your age: "
+age1 = gets().chomp()
 
-num = -20
-puts num.abs()
+#tanpa chomp code akan melakukan new line
 
-num = 20.74563
-puts num.round()
-
-num = 20.24563
-puts num.round()
-
-num = 20.1
-puts num.ceil()
-
-num = 20.9
-puts num.floor()
-
-puts Math.sqrt(36)
-
-puts Math.log(1)
-
-puts 1 + 7
-puts 1.0 + 7
-puts 10 / 7
-puts 10 / 7.0
+puts ("Hello, my name is " + name + ", And I am " + age + " years old")
+puts ("Hello, my name is " + name1 + ", And I am " + age1 + " years old")
