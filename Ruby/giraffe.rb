27d2af4c88d06c0e027f10c9514f7494c032540a
reza <sebutsajamentari@gmail.com>
2019-09-07 11:00:07 +0700

    Building mad labs game

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,18 +1,10 @@
-puts "Enter the first number: "
-num1 = gets().chomp()
-puts "Enter the second number: "
-num2 = gets().chomp()
+puts "Enter a color: "
+color = gets().chomp()
+puts "Enter a plural noun: "
+plural_noun = gets().chomp()
+puts "Enter a celebrity: "
+celebrity_name = gets().chomp()
 
-puts "Enter the third number: "
-num3 = gets().chomp()
-puts "Enter the fourth number: "
-num4 = gets().chomp()
-
-puts "Enter the fifth number: "
-num5 = gets().chomp().to_i#dapat mengubah melalui ini
-puts "Enter the sixth number: "
-num6 = gets().chomp().to_i
-
-puts (num1 + num2)
-puts (num3.to_i + num4.to_i)#ataupun ini
-puts (num5.to_i + num6.to_i)
+puts ("Roses are " + color)
+puts ( + plural_noun + " are blue")
+puts ("I love " + celebrity_name)
