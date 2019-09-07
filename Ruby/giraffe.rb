7c8f3b320e08a704d8e368953ce622c27ea3f03e
reza <sebutsajamentari@gmail.com>
2019-09-07 12:06:45 +0700

    Arrays

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,10 +1,27 @@
-puts "Enter a color: "
-color = gets().chomp()
-puts "Enter a plural noun: "
-plural_noun = gets().chomp()
-puts "Enter a celebrity: "
-celebrity_name = gets().chomp()
+friends = Array["Kevin", "Oscar", "Andy"]
+puts friends
 
-puts ("Roses are " + color)
-puts ( + plural_noun + " are blue")
-puts ("I love " + celebrity_name)
+alltype = Array[1, "Kevin", false]
+puts alltype
+
+puts friends[0]
+puts friends[-1]
+puts friends[0,2]
+
+puts friends.length()
+puts friends.include? "Oscar"
+puts friends.reverse()
+puts friends.sort()
+
+friends[0] = "Reza"
+puts friends[0]
+
+#dinamis
+teman = Array.new
+
+teman[0] = "Asep"
+teman[5] = "Cecep"
+
+puts teman[0]
+puts teman[5]
+puts teman
