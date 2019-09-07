15c1f4d3780de9b6dfe7f5e7fc40208810076463
reza <sebutsajamentari@gmail.com>
2019-09-07 10:54:26 +0700

    Building a calculator

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,14 +1,18 @@
-puts "Enter your name: "
-name = gets()
-puts "Enter your age: "
-age = gets()
+puts "Enter the first number: "
+num1 = gets().chomp()
+puts "Enter the second number: "
+num2 = gets().chomp()
 
-puts "Enter your name: "
-name1 = gets().chomp()
-puts "Enter your age: "
-age1 = gets().chomp()
+puts "Enter the third number: "
+num3 = gets().chomp()
+puts "Enter the fourth number: "
+num4 = gets().chomp()
 
-#tanpa chomp code akan melakukan new line
+puts "Enter the fifth number: "
+num5 = gets().chomp().to_i#dapat mengubah melalui ini
+puts "Enter the sixth number: "
+num6 = gets().chomp().to_i
 
-puts ("Hello, my name is " + name + ", And I am " + age + " years old")
-puts ("Hello, my name is " + name1 + ", And I am " + age1 + " years old")
+puts (num1 + num2)
+puts (num3.to_i + num4.to_i)#ataupun ini
+puts (num5.to_i + num6.to_i)
