788d6ef2d350d40aec4bc834151846a51d0fc8ce
reza <sebutsajamentari@gmail.com>
2019-09-08 22:10:01 +0700

    Building a guessing game

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,6 +1,21 @@
-index = 1
+guessing_word = "giraffe"
+guess = ""
+guess_count = 0
+out_of_guess = false
+guess_limit = 3
 
-while index <= 8
-  puts index
-  index += 1
+while guess != guessing_word and !out_of_guess
+  if guess_count < guess_limit
+  puts "Enter a guess: "
+  guess = gets().chomp()
+  guess_count += 1
+  else
+    out_of_guess = true
+  end
+end
+
+if out_of_guess
+  puts "You Lose !"
+else
+  puts "You Won !"
 end
