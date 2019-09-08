9eee5786e7019a6c2ca0082a9494ca7b02d8f1cc
reza <sebutsajamentari@gmail.com>
2019-09-08 15:28:30 +0700

    Building a better calculator

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,16 +1,18 @@
-def max(num1, num2, num3)
-   if num1 >= num2 and num1 >= num3
-    return num1
-   elsif num2 >= num1 and num2 >= num3
-    return num2
-   elsif num3 >= num1 and num3 >= num2
-    return num3
-  end
+puts "Enter first number: "
+num1 = gets().chomp().to_f
+puts "Enter an operator: "
+op = gets().chomp()
+puts "Enter second number: "
+num2 = gets().chomp().to_f
+
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "/"
+  puts (num1 / num2)
+elsif op == "*"
+  puts (num1 * num2)
+else
+  puts "Invalid Operator"
 end
-
-puts max(100, 20, 5)
-puts max(15, 80, 3)
-puts max(1, 20, 50)
-
-#operator
-# = < > <= >= !=
