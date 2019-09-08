c7c175a88bdc86b7c3f5f955400176120dd9934b
reza <sebutsajamentari@gmail.com>
2019-09-08 21:47:35 +0700

    While expression

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,32 +1,6 @@
-def get_day_name(day)
+index = 1
 
-  day_name = ""
-
-  case day
-  when "mon"
-    day_name =  "Monday"
-  when "tue"
-    day_name = "Tuesday"
-  when "wed"
-    day_name = "Wednesday"
-  when "thu"
-    day_name = "Thursday"
-  when "fri"
-    day_name = "Friday"
-  when "sat"
-    day_name = "Saturday"
-  when "sun"
-    day_name = "Sunday"
-  else
-    "Invalid Abbreviation"
-  end
-    return day_name
+while index <= 8
+  puts index
+  index += 1
 end
-
-puts get_day_name ("sun")
-puts get_day_name ("mon")
-puts get_day_name ("tue")
-puts get_day_name ("wed")
-puts get_day_name ("thu")
-puts get_day_name ("fri")
-puts get_day_name ("sat")
