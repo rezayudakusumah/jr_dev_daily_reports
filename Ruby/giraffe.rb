7493d83fe6c2302fda2893052c4ada494180eeaa
reza <sebutsajamentari@gmail.com>
2019-09-08 21:43:00 +0700

    Case expressions

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,18 +1,32 @@
-puts "Enter first number: "
-num1 = gets().chomp().to_f
-puts "Enter an operator: "
-op = gets().chomp()
-puts "Enter second number: "
-num2 = gets().chomp().to_f
+def get_day_name(day)
 
-if op == "+"
-  puts (num1 + num2)
-elsif op == "-"
-  puts (num1 - num2)
-elsif op == "/"
-  puts (num1 / num2)
-elsif op == "*"
-  puts (num1 * num2)
-else
-  puts "Invalid Operator"
+  day_name = ""
+
+  case day
+  when "mon"
+    day_name =  "Monday"
+  when "tue"
+    day_name = "Tuesday"
+  when "wed"
+    day_name = "Wednesday"
+  when "thu"
+    day_name = "Thursday"
+  when "fri"
+    day_name = "Friday"
+  when "sat"
+    day_name = "Saturday"
+  when "sun"
+    day_name = "Sunday"
+  else
+    "Invalid Abbreviation"
+  end
+    return day_name
 end
+
+puts get_day_name ("sun")
+puts get_day_name ("mon")
+puts get_day_name ("tue")
+puts get_day_name ("wed")
+puts get_day_name ("thu")
+puts get_day_name ("fri")
+puts get_day_name ("sat")
