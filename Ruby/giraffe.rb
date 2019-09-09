b95b8336c338752a9eeb4d19c178b81ffc1b8436
reza <sebutsajamentari@gmail.com>
2019-09-09 10:48:03 +0700

    for loops

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,21 +1,17 @@
-guessing_word = "giraffe"
-guess = ""
-guess_count = 0
-out_of_guess = false
-guess_limit = 3
+friends = ["Kevin", "Oscar", "Andy", "Jonathan", "Eric"]
 
-while guess != guessing_word and !out_of_guess
-  if guess_count < guess_limit
-  puts "Enter a guess: "
-  guess = gets().chomp()
-  guess_count += 1
-  else
-    out_of_guess = true
-  end
+for friend in friends
+  puts friend
 end
 
-if out_of_guess
-  puts "You Lose !"
-else
-  puts "You Won !"
+friends.each do |friend|
+  puts friend
+end
+
+for index in 0..5
+  puts index
+end
+
+8.times do |index| #can be anything
+  puts index
 end
