3cf0fa0bdb89360e78647c57d065e65d2f6d2531
reza <sebutsajamentari@gmail.com>
2019-09-10 09:41:14 +0700

    Object methods

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,54 +1,23 @@
-class Book
-  attr_accessor :title, :author, :pages
-end
+class Student
+  attr_accessor :name, :major, :gpa
 
-book1 = Book.new()
-book1.title = "Harry Potter"
-book1.author = "JK Rowling"
-book1.pages = 400
+  def initialize(name, major, gpa)
+    @name = name
+    @major = major
+    @gpa = gpa
+  end
 
-puts book1.title
-puts book1.author
-puts book1.pages
-
-book2 = Book.new()
-book2.title = "Incarceron"
-book2.author ="Reza"
-book2.pages = 500
-
-puts book2.title
-puts book2.author
-puts book2.pages
-
-class Hello
-  attr_accessor :title, :author, :pages
-  def initialize()
-    puts "hello"
+  def has_honors
+    if @gpa >= 3.5
+      return true
+    else
+      return false
+    end
   end
 end
 
-hello1 = Hello.new()
+student1 = Student.new("Reza", "IT", 3.6)
+student2 = Student.new("Mike", "Business", 2.5)
 
-class Hello_Nama
-  attr_accessor :title, :author, :pages
-  def initialize(name)
-    puts ("hello " + name)
-  end
-end
-
-hello_nama1 = Hello_Nama.new("Mike")
-
-class Booklet
-  attr_accessor :title, :author, :pages
-  def initialize(title, author, pages)
-    @title = title
-    @author = author
-    @pages = pages
-  end
-end
-
-booklet1 = Booklet.new("Incarceron", "Reza", "500")
-booklet2 = Booklet.new("Dua Dunia", "Reza", "600")
-
-puts booklet1.title
-puts booklet2.pages
+puts student1.has_honors
+puts student2.has_honors
