d0d76e1c2e8022c5867503c91924bcaf71135496
reza <sebutsajamentari@gmail.com>
2019-09-10 09:30:04 +0700

    Classes & Object + Initialize

--- Ruby/giraffe.rb
+++ Ruby/giraffe.rb
@@ -1,12 +1,54 @@
-
-File.open("Ruby/employees.txt", "a") do |file|
-
-  file.write("\nOscar, Accounting")
-
+class Book
+  attr_accessor :title, :author, :pages
 end
 
-File.open("Ruby/index.html", "w") do |file|
+book1 = Book.new()
+book1.title = "Harry Potter"
+book1.author = "JK Rowling"
+book1.pages = 400
 
-  file.write("<h1>Hello</h1>")
+puts book1.title
+puts book1.author
+puts book1.pages
 
+book2 = Book.new()
+book2.title = "Incarceron"
+book2.author ="Reza"
+book2.pages = 500
+
+puts book2.title
+puts book2.author
+puts book2.pages
+
+class Hello
+  attr_accessor :title, :author, :pages
+  def initialize()
+    puts "hello"
+  end
 end
+
+hello1 = Hello.new()
+
+class Hello_Nama
+  attr_accessor :title, :author, :pages
+  def initialize(name)
+    puts ("hello " + name)
+  end
+end
+
+hello_nama1 = Hello_Nama.new("Mike")
+
+class Booklet
+  attr_accessor :title, :author, :pages
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
+end
+
+booklet1 = Booklet.new("Incarceron", "Reza", "500")
+booklet2 = Booklet.new("Dua Dunia", "Reza", "600")
+
+puts booklet1.title
+puts booklet2.pages
